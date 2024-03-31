cdd365b2011545fff0ef93462a6936ff85ff39db
Marc <14913675+MarcStdt@users.noreply.github.com>
2024-03-31 08:16:46 +0200

    fix: worbench.ts build

--- workbench.ts
+++ workbench.ts
@@ -28,6 +28,7 @@ export function getCookie(name: string) {
   if (parts.length == 2) {
     return parts.pop()?.split(";").shift();
   }
+  return null;
 }
 
 export function deleteCookie(name: string) {
